03f5bc833320683bf9ff72765589da8679eed1a4
Сергей Андреев <aandreev.sm@gmail.com>
2020-01-18 06:28:28 +0300

    Updates

---
+++ precache-manifest.d0304db2205b8268c0949a3e9e54e108.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "436a7f917d1760cb6db40ac387c33ae8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c26ce7e8450c8a235ae3",
+    "url": "/static/css/main.2be2fdd6.chunk.css"
+  },
+  {
+    "revision": "a9ea9e461a102a14dee4",
+    "url": "/static/js/2.43f1f0d9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.43f1f0d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c26ce7e8450c8a235ae3",
+    "url": "/static/js/main.8446c8ba.chunk.js"
+  },
+  {
+    "revision": "9db003f56326e066661b",
+    "url": "/static/js/runtime-main.3707882d.js"
+  }
+]);
